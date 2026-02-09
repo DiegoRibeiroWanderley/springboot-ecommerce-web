fcfc4d556675b9e68cbcfac6853962c5fd18ef72
Diego <eliwandejp@gmail.com>
2026-02-09 16:37:42 -0300

    ProductCard Component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,65 @@
-const ProductCard = () => {
+import { useState } from "react"
+import { FaShoppingCart } from "react-icons/fa";
+
+const ProductCard = ({
+    productId,
+    productName,
+    image,
+    description,
+    quantity,
+    price,
+    discount,
+    specialPrice
+}) => {
+    const [openProductViewModel, setOpenProductViewModel] = useState(false)
+    const buttonLoader = false;
+    const [selectedViewProduct, setSelectedViewProduct] = useState("")
+    const isAvailable = quantity && Number(quantity) > 0
+
     return (
-        <div className="bg-pink-200 rounded-lg p-2">
-            <h1>Product</h1>
+        <div className="rounded-lg border-0 shadow-xl overflow-hidden transition-shadow duration-300">
+            <div onClick={() => {}} className="w-full overflow-hidden aspect-3/2">
+                <img className="w-full h-full cursor-pointer transition-transform duration-300 transform hover:scale-105"
+                    src={image}
+                    alt={productName}>
+                </img>
+            </div>
+            <div className="p-4">
+                <h2 onClick={() => {}}
+                    className="text-lg font-semibold mb-2 cursor-pointer">
+                    {productName}
+                </h2>
+
+                <div className="min-h-20 max-h-20">
+                    <p className="text-gray-600 text-sm">{description}</p>
+                </div>
+                
+                <div className="relative flex items-center justify-between">
+                    {specialPrice !== price ? (
+                        <div className="flex flex-col">
+                            <span className="text-gray-400 line-through">
+                                ${Number(price).toFixed(2)}
+                            </span>
+                            <span className="text-xl font-bold text-slate-700">
+                                ${Number(specialPrice).toFixed(2)}
+                            </span>
+                        </div>
+                    ) : (
+                        <div className="flex flex-col relative">
+                            <span className="absolute top-6 text-xl font-bold text-gray-700 p">
+                                ${Number(price).toFixed(2)}
+                            </span>
+                        </div>
+                    )}
+
+                    <button className={`bg-pink-200 p-1 rounded-lg transition-colors duration-300 w-36 flex justify-center absolute top-6 right-0 text-gray-800
+                                        ${isAvailable ? "opacity-100 hover:bg-pink-500" : "opacity-70"}`}>
+                        <FaShoppingCart className="mr-2 absolute top-2 left-3"/>
+                        <h1 className="ml-5">{isAvailable ? "Add to Cart" : "Stock Out"}</h1>
+                    </button>
+                </div>
+
+            </div>
         </div>
     )
 }

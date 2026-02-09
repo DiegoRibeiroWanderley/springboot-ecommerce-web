6f4fb6e08cb6f22c683f207481a379b8b1fb1b61
Diego <eliwandejp@gmail.com>
2026-02-08 22:54:00 -0300

    Project created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 class="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
+        Welcome
+      </h1>
+    </div>
+  )
+}
+
+export default App

08cde34cdfddceec30eac7b8d51366dd9fbd2bd3
Diego <eliwandejp@gmail.com>
2026-02-19 23:08:01 -0300

    Implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/home/Home'
 import Products from './components/products/Products'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <>
-      <Products />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Home /> }/>
+        <Route path='/products' element={ <Products /> }/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

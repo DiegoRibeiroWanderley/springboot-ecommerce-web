d77bbc9fbeeb03a4b8ee812df825cdce1d826167
Diego <eliwandejp@gmail.com>
2026-02-20 22:58:47 -0300

    NavigationBar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/home/Home'
 import Products from './components/products/Products'
+import NavigationBar from './components/shared/NavigationBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
+      <NavigationBar />
       <Routes>
         <Route path='/' element={ <Home /> }/>
         <Route path='/products' element={ <Products /> }/>

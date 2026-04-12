3e4593d2e7215967ca8707530ff61758fe25f9d1
Diego <eliwandejp@gmail.com>
2026-04-12 17:24:50 -0300

    AddressInfo component for checkout

---
+++ src/components/checkout/AddressInfo.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react"
+import Spinner from "../shared/Spinner"
+import { FaAddressBook } from "react-icons/fa"
+import AddressInfoModal from "./AddressInfoModal"
+import AddAddressForm from "./AddAddressForm"
+
+const AddressInfo = () => {
+
+    const [openAddressModal, setOpenAddressModal] = useState(false)
+    const [selectedAddress, setSelectedAddress] = useState("")
+    const addNewAddressHandler = () => {
+        setSelectedAddress("")
+        setOpenAddressModal(true)
+    }
+
+    const noAddressExist = true
+    const isLoading = false
+
+  return (
+    <div className='pt-4'>
+        {noAddressExist ? (
+            <div className='p-6 rounded-lg max-w-md mx-auto flex flex-col items-center justify-center'>
+                <FaAddressBook size={50} className="text-gray-500 mb-4"/>
+                <h1 className='text-slate-900 text-center font-semibold text-2xl mb-2'>
+                    No address added yet
+                </h1>
+                <p className='text-slate-800 text-center mb-6'>
+                    Please add your address to complete purchase
+                </p>
+
+                <button
+                 className="px-4 py-2 bg-pink-300 text-white font-medium rounded hover:bg-pink-400 transition-all"
+                 onClick={addNewAddressHandler}>
+                    Add address
+                </button>
+            </div>
+        ) : (
+            <div className='relative p-6 rounded-lg max-w-md mx-auto'>
+                <h1 className='text-slate-800 text-center font-bold text-2xl'>
+                    Select Address
+                </h1>
+
+                {isLoading ? (
+                    <div className='justify-center mt-40 ml-11'>
+                        <Spinner size={300} />
+                    </div>
+                ) : (
+                    <div></div>
+                )}
+            </div>
+        )}
+
+        <AddressInfoModal
+            open={openAddressModal}
+            setOpen={setOpenAddressModal}>
+                <AddAddressForm address = {selectedAddress} setOpenAddressModal={setOpenAddressModal}/>
+        </AddressInfoModal>
+    </div>
+  )
+}
+
+export default AddressInfo

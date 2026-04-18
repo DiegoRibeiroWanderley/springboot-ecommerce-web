406504c196bd1968d6618bae8f01aebb461f6fbf
Diego <eliwandejp@gmail.com>
2026-04-18 16:41:45 -0300

    Showing error messages

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getUserAddresses } from "../../store/actions";
 import toast from "react-hot-toast";
 import Spinner from "../shared/Spinner";
+import ErrorPage from "../shared/ErrorPage";
 
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
@@ -83,6 +84,7 @@ const Checkout = () => {
           </Button>
         )}
       </div>
+      {errorMessage && <ErrorPage message={errorMessage} />}
     </div>
   );
 };
